bc2733c4983d543cf469236cb5aba970fc8af2d5
Doug Cook (WINDOWS) <dcook@microsoft.com>
2023-06-29 14:12:21 -0700

    Rust Guid: fix as_bytes_raw and borrow

--- etw/rust/tracelogging_dynamic/src/changelog.rs
+++ etw/rust/tracelogging_dynamic/src/changelog.rs
@@ -3,6 +3,15 @@
 #[allow(unused_imports)]
 use crate::*; // For docs
 
+/// # v1.2.1 (2023-06-29)
+/// - Guid: Fix `as_bytes_raw()` method and `borrow<[u8; 16]>` trait.
+pub mod v1_2_1 {}
+
+/// # v1.2.0 (2023-05-15)
+/// - Guid: Added `as_bytes_raw()` method and `borrow<[u8; 16]>` trait.
+/// - Opcode enum names clarified.
+pub mod v1_2_0 {}
+
 /// # v1.1.0 (2023-03-24, Breaking)
 /// - **Breaking:** Move [`Provider`] configuration parameters from
 ///   [`Provider::register`] to [`Provider::new`]. This allows
